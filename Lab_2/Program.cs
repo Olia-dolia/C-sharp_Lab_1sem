9517c15e8ab921dd7d8da42a3d4a09716b10dc5c
Olia-dolia <71394812+Olia-dolia@users.noreply.github.com>
2020-09-16 20:58:26 +0300

    Add files via upload

---
+++ Lab_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                int k;
+                double a = 0;
+                try
+                {
+                    Console.WriteLine("\tВведите k(k > 0) ");
+                    k = int.Parse(Console.ReadLine());
+                    while (k <= 0)
+                    {
+                        Console.WriteLine("Вы неверно ввели k! Попробуйте еще раз!");
+                        k = int.Parse(Console.ReadLine());
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не удалось преобразовать строку в число!");
+                    Console.WriteLine("Нажмите ENTER, чтобы продолжить!");
+                    Console.ReadLine();
+                    continue;
+                }
+                for (int n = 0; n <= k; n++)
+                {
+                    a += Math.Pow(k,2)/ (Math.Pow(-1, k) * Math.Pow(k, 2) + 5);
+                }
+                Console.WriteLine("Сумма ряду = " + a);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
