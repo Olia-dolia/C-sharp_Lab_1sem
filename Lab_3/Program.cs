da73e85aac74de032d7fecb5a59f4d37bfac5411
Olia-dolia <71394812+Olia-dolia@users.noreply.github.com>
2020-09-28 19:32:45 +0300

    Add files via upload

---
+++ Lab_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //16. Перевірити істинність вислову: "Цифри даного тризначного числа утворюють геометричну прогресію"
+            while (true)
+            {
+                Console.Clear();
+                int number;
+                double q;
+                int[] arr = new int[3];
+                bool Geometric = false;
+                Console.WriteLine("Введите целое трехзначное число: ");
+                try //проверка на input
+                {
+                    number = int.Parse(Console.ReadLine()); 
+                    while (number < 100 || number > 1000)
+                    {
+                        Console.WriteLine("Вы неверно ввели число! Попробуйте еще раз!");
+                        number = int.Parse(Console.ReadLine());
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не удалось преобразовать строку в число!");
+                    Console.WriteLine("Нажмите ENTER, чтобы продолжить!");
+                    Console.ReadLine();
+                    continue;
+                }
+                for(int i = 2; i>-1; i--)//запись в массив числа
+                {
+                    arr[i]= number % 10;
+                    number = number /= 10;
+                }
+
+                q = arr[1] / arr[0]; //нахождение знаменателя прогрессии
+                if (arr[1] == 0)//на 0 делить нельзя
+                {
+                    Console.WriteLine("На ноль делить нельзя! Прогрессия не существует. Нажмите Enter");
+                } else if (arr[1] / arr[0] == q && arr[2] / arr[1] == q) {//проверка элементов
+                    Geometric = true;
+                    Console.WriteLine(Geometric);
+                } else {
+                    Console.WriteLine(Geometric);
+                }
+                
+                Console.ReadLine();
+               
+            }
+        }
+    }
+}
