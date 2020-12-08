e10509f57d5a4311fa43df2fdf0ee91cf310891e
Olia-dolia <71394812+Olia-dolia@users.noreply.github.com>
2020-12-08 15:24:15 +0200

    Add files via upload

---
+++ Lab_8/Lab_8_part2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_part2
+{
+    class Program
+    {
+        class Disk
+        {
+            public long number;
+            public string function = "Вы можете использовать такие символы: от 0 до 9";
+            public void makeCall(long number)
+            {
+                Console.WriteLine("*Beeping*");
+            }
+            public void answerCall()
+            {
+                Console.WriteLine("*Ringing*");
+            }
+        }
+        class PushButton : Disk
+        {
+            public string Name = "";
+     
+            public PushButton() {
+                function = "Вы можете использовать такие символы: от 0 до 9, +, *, #, A-Z, a-z";
+            }
+        }
+        class MobileBW : PushButton
+        {
+            public MobileBW()
+            {
+                function = "Вы можете использовать такие символы: от 0 до 9, +, -, *, #, A-Z, a-z, (), ?, !";
+            }
+
+            public void sendSMS(long number, string text)
+            {
+                Console.WriteLine("{1} sent to {0}", number, text);
+            }
+            public void receiveSMS(long number, string text)
+            {
+                Console.WriteLine("{0} : {1}", number, text);
+            }
+        }
+        class MobileColor : MobileBW
+        {
+            public int amountOfColors;
+            public int simCards;
+
+            public void sendMMS(long number)
+            {
+                Console.WriteLine(" sent to {0}", number);
+            }
+            public void receiveMMS(long number)
+            {
+                Console.WriteLine("{0} : ", number);
+            }
+        }
+        class Smartphone : MobileColor
+        {
+            const bool sensor = true;
+            public int amountOfTouches;
+            public int amountOfCameras;
+            public string cameraCharacteristics;
+
+            public void TakePhoto()
+            {
+                Console.WriteLine("*Shutter Sound*");
+            }
+            public void TakeWidePhoto()
+            {
+                Console.WriteLine("*Shutter Sound*");
+            }
+            public void TakePortraitPhoto()
+            {
+                Console.WriteLine("*Shutter Sound*");
+            }
+            public void MakeVideo()
+            {
+                Console.WriteLine("*Recording*");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
---
+++ Lab_8/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    class Program
+    {
+        public class Garage : IEnumerable, IEnumerator
+        {
+            protected int size;
+            protected int index;
+            public AUTO[] carArray;
+            private int pos = -1;
+
+            public Garage()
+            {
+                carArray = new AUTO[0];
+            }
+
+            public void AddCar(string car, int sp, string col, int year)
+            {
+                Array.Resize(ref carArray, carArray.Length + 1);
+                carArray[carArray.Length - 1] = new AUTO(car, sp, col, year);
+            }
+            
+            public AUTO this[int index]
+            {
+                get { return carArray[index]; }
+            }
+
+            public int Count
+            {
+                get { return carArray.Length; }
+            }
+
+            #region Члены IEnumerable
+
+            public IEnumerator GetEnumerator()
+            {
+                return carArray.GetEnumerator();
+            }
+            #endregion
+
+            #region Члены IEnumerator
+            public bool MoveNext()
+            {
+                if (pos < carArray.Length)
+                {
+                    pos++;
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            public void Reset()
+            {
+                pos = 0;
+            }
+
+            public object Current
+            {
+                get { return carArray[pos]; }
+            }
+
+            #endregion
+
+
+        }
+
+        public class AUTO
+        {
+            private string model;
+            private int speed;
+            private string color;
+            private int yearOfIssue;
+
+            public AUTO(string model, int speed, string color, int yearOfIssue)
+            {
+                this.model = model;
+                this.speed = speed;
+                this.color = color;
+                this.yearOfIssue = yearOfIssue;
+            }
+
+            public string Model
+            {
+                get { return this.model; }
+            }
+
+            public int Speed
+            {
+                get { return this.speed; }
+            }
+            public string Color
+            {
+                get { return this.color; }
+            }
+            public int YearOfIssue
+            {
+                get { return this.yearOfIssue; }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество машин в гараже: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Garage garage = new Garage();
+            while (true)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Модель машины: ");
+                    string car = Console.ReadLine();
+                    Console.Write("Скорость машины: ");
+                    int sp = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Цвет машины: ");
+                    string col = Console.ReadLine();
+                    Console.Write("Год выпуска машины: ");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    garage.AddCar(car, sp, col, year);
+                }
+
+                Console.WriteLine("\n");
+                Console.WriteLine("Просмотр машин в гараже");
+
+                foreach (AUTO car in garage)
+                {
+                    Console.WriteLine("Модель - {0}, Скорость - {1}, Цвет - {2}, Год выпуска - {3}", car.Model, car.Speed, car.Color, car.YearOfIssue);
+                }
+                Console.ReadLine();
+                Console.Write("Если хотите добавить машину в гараж нажмите - 1: ");
+                int choose = int.Parse(Console.ReadLine());
+               if( choose == 1)
+                {
+                    {
+                        Console.Write("Модель машины: ");
+                        string car = Console.ReadLine();
+                        Console.Write("Скорость машины: ");
+                        int sp = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Цвет машины: ");
+                        string col = Console.ReadLine();
+                        Console.Write("Год выпуска машины: ");
+                        int year = Convert.ToInt32(Console.ReadLine());
+                        garage.AddCar(car, sp, col, year);
+                    }
+                    foreach (AUTO car in garage)
+                    {
+                        Console.WriteLine("Модель - {0}, Скорость - {1}, Цвет - {2}, Год выпуска - {3}", car.Model, car.Speed, car.Color, car.YearOfIssue);
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
