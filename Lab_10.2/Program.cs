4b0cb91832917e8104abc5da0f7baaab81b2232c
Olia-dolia <71394812+Olia-dolia@users.noreply.github.com>
2020-12-19 16:45:05 +0200

    Add files via upload

---
+++ Lab_10.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_2//Variant 4
+{
+    public static class DoubleArrayExtention
+    {
+        public static double AvarageOfArray(this double[] arr)
+        {
+            double temp = 0.0;
+            
+            for(int i=0; i <arr.Length; i++)
+            {
+                temp += arr[i];
+            }
+           return temp / arr.Length;
+            
+        }
+        public static double AvarageOfarray_2(this double[] arr)
+        {
+            double temp2 = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp2 *= arr[i];
+            }
+            return temp2 / arr.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+
+            double[] arr = new double[n] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            double avarageAr = 0;
+            double avarageGeom = 0;
+            Console.Write("Массив:\t");
+            foreach (int i in arr)
+            {
+                Console.Write(i + "\t");
+            }
+           
+            avarageAr = arr.AvarageOfArray();
+            avarageGeom = arr.AvarageOfarray_2();
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое: " + avarageAr + "\t" + "Среднее геометрическое: " + avarageGeom);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_10/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    public static class CharArrayExtension
+    {
+        public static void Reverse(this char[] value1, char[] value2)
+        {
+            for (int i = value1.Length - 2; i > -1; i--)
+            {
+                Console.Write(value1[i]);
+            }
+            Console.Write(',');
+            for (int i = value2.Length - 2; i > -1; i--)
+            {
+                Console.Write(value2[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void ReverseR(this char[] value1, char[] value2, int i1, int i2)
+        {
+            ReverseR(value1, i1);
+            Console.Write(',');
+            ReverseR(value2, i2);
+
+        }
+        public static void ReverseR(this char[] value, int i)
+        {
+            if (i > -1)
+            {
+                Console.Write(value[i]);
+                ReverseR(value, i - 1);
+            }
+        }
+    }
+        public static class StringExtension
+    {
+        public static void Reverse(this string value)
+        {
+            for (int i = value.Length - 1; i > -1; i--)
+            {
+                Console.Write(value[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void ReverseR(this string value, int i)
+        {
+            if (i > -1)
+            {
+                Console.Write(value[i]);
+                ReverseR(value, i - 1);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;    
+            Console.InputEncoding = System.Text.Encoding.Unicode;      
+
+            Console.WriteLine("Введіть число або рядок (без розділових знаків та пробілів):");
+            string data = Console.ReadLine();
+
+            Console.WriteLine("\nРезультат:");
+            data.Reverse();
+            Console.WriteLine("Рекурсія:");
+            data.ReverseR(data.Length - 1);
+
+            char[] chData = new char[data.Length + 1];
+            for (int i = 0; i < data.Length; i++)
+            {
+                chData[i] = data[i];
+            }
+
+            Console.WriteLine("\n\nВведіть дробове число або рядок з ОДНІЄЮ комою всередині (без пробілів):");
+            string data1 = Console.ReadLine();
+
+            int size = 0;
+            for (int i = 0; i < data1.Length; i++)
+            {
+                if (data1[i] == ',')
+                {
+                    size = i;
+                    break;
+                }
+            }
+
+            char[] data1_1 = new char[size + 1];
+            char[] data1_2 = new char[data1.Length - size];
+
+            for (int i = 0; i < size; i++)
+            {
+                data1_1[i] = data1[i];
+            }
+            for (int i = size + 1, j = 0; i < data1.Length; i++, j++)
+            {
+                data1_2[j] = data1[i];
+            }
+
+            Console.WriteLine("\nРезультат:");
+            data1_1.Reverse(data1_2);
+            Console.WriteLine("Рекурсія:");
+            data1_1.ReverseR(data1_2, data1_1.Length - 2, data1_2.Length - 2);
+
+            Console.ReadKey();
+        }
+    }
+}
